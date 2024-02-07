4a4fe620730889fb58aeb777938567c657471375
Mike Smith <msmith261986@gmail.com>
2024-02-07 10:42:35 -0600

    Update TitleBar.jsx

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function TitleBar() {
     return (
         <nav className="titleBar">
-            <img src="../src/assets/worldwide.png" className="nav--logo" />
+            <img src="assets/worldwide.png" className="nav--logo" />
             <p>my travel journal</p>
         </nav>
     )
